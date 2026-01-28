4ea0dd5b2f766d910b7acc303a5c3bdbb3183862
롱롱(Rongrong) <107838130+rjsej12@users.noreply.github.com>
2026-01-28 16:43:36 +0900

    feat(metrics): add Prometheus metrics to processor

--- internal/worker/processor.go
+++ internal/worker/processor.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"log"
 	"time"
+
+	"github.com/rjsej12/sentinel-go/internal/metrics"
 )
 
 type Processor struct {
@@ -28,6 +30,23 @@ func (p *Processor) Start() {
 		go p.worker(i)
 	}
 	log.Printf("Processor started with %d workers", p.workerPool)
+
+	metrics.WorkerQueueCapacity.Set(float64(p.queue.Capacity()))
+	go p.updateQueueMetrics()
+}
+
+func (p *Processor) updateQueueMetrics() {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-p.ctx.Done():
+			return
+		case <-ticker.C:
+			metrics.WorkerQueueLength.Set(float64(p.queue.Length()))
+		}
+	}
 }
 
 func (p *Processor) worker(id int) {
@@ -45,6 +64,7 @@ func (p *Processor) worker(id int) {
 					return
 				}
 				log.Printf("Worker %d: error dequeing: %v", id, err)
+				metrics.WorkerErrorsTotal.WithLabelValues("dequeue").Inc()
 				continue
 			}
 
@@ -55,29 +75,51 @@ func (p *Processor) worker(id int) {
 
 func (p *Processor) processJob(workerID int, job Job) {
 	start := time.Now()
+
+	metrics.WorkerJobsInFlight.Inc()
+	defer metrics.WorkerJobsInFlight.Dec()
+
 	log.Printf("Worker: %d: processing job %s (type: %s)", workerID, job.ID, job.Type)
+
+	var err error
+	jobTypeStr := string(job.Type)
 
 	switch job.Type {
 	case JobTypeProcess:
-		p.handleProcessJob(job)
+		err = p.handleProcessJob(job)
 	case JobTypeDelay:
-		p.handleDelayJob(job)
+		err = p.handleDelayJob(job)
 	default:
 		log.Printf("Worker: %d: unknown job type: %s", workerID, job.Type)
+		metrics.WorkerErrorsTotal.WithLabelValues("unknown_type").Inc()
+		metrics.WorkerJobsProcessedTotal.WithLabelValues(jobTypeStr, "error").Inc()
+		return
 	}
 
 	duration := time.Since(start)
-	log.Printf("Worker: %d: completed job %s in %v", workerID, job.ID, duration)
+
+	metrics.WorkerJobDurationSeconds.WithLabelValues(jobTypeStr).Observe(duration.Seconds())
+
+	if err != nil {
+		metrics.WorkerJobsProcessedTotal.WithLabelValues(jobTypeStr, "error").Inc()
+		metrics.WorkerErrorsTotal.WithLabelValues("process").Inc()
+		log.Printf("Worker: %d: error processing job %s: %v", workerID, job.ID, err)
+	} else {
+		metrics.WorkerJobsProcessedTotal.WithLabelValues(jobTypeStr, "success").Inc()
+		log.Printf("Worker: %d: completed job %s in %v", workerID, job.ID, duration)
+	}
 }
 
-func (p *Processor) handleProcessJob(job Job) {
+func (p *Processor) handleProcessJob(job Job) error {
 	processTime := 100 + (len(job.Data) % 400)
 	time.Sleep(time.Duration(processTime) * time.Millisecond)
+	return nil
 }
 
-func (p *Processor) handleDelayJob(job Job) {
+func (p *Processor) handleDelayJob(job Job) error {
 	delayTime := 1000 + (len(job.Data) % 2000)
 	time.Sleep(time.Duration(delayTime) * time.Millisecond)
+	return nil
 }
 
 func (p *Processor) Stop() {
